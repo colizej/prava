37d4f582c893816c3d9438b419bcf6bfaefd81d1
colizej <colizej@gmail.com>
2026-03-06 21:47:49 +0100

    fix: custom 404/500 handlers pointing to templates/errors/

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,24 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.shortcuts import render
 
 from apps.blog.sitemaps import BlogSitemap, BlogCategorySitemap
 from apps.main.sitemaps import StaticViewSitemap
 from apps.reglementation.sitemaps import ArticleSitemap, ReglCategorySitemap
 from apps.examens.sitemaps import ExamCategorySitemap, QuestionSitemap
+
+
+def handler404_view(request, exception):
+    return render(request, 'errors/404.html', status=404)
+
+
+def handler500_view(request):
+    return render(request, 'errors/500.html', status=500)
+
+
+handler404 = handler404_view
+handler500 = handler500_view
 
 sitemaps = {
     'static': StaticViewSitemap,
